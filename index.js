eb33e7fa69e81e5f5a6b6a41d2f3b558953eb715
Stuart Mumford <stuart@cadair.com>
2022-05-06 13:30:17 +0100

    hmm

--- index.js
+++ index.js
@@ -29,14 +29,15 @@ async function gatherPreviousJobStatus() {
     const workflow = await octokit.rest.actions.listJobsForWorkflowRun({...context.repo,
                                                                         run_id: context.runId,
                                                                         filter:'latest'});
-    core.info(util.inspect(workflow.data.jobs, colors=true, depth=null));
 
-    const currentJob = workflow.data.jobs.filter(
+    const currentJob = workflow.data.jobs.find(
         (job) => {
             return job.name === context.job;
         }
     );
     core.info(util.inspect(currentJob, colors=true, depth=null));
+
+
 }
 
 
