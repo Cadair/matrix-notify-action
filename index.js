b9a2d3b40f6356410f26f9749eef14cd4f3b8ad7
Stuart Mumford <stuart@cadair.com>
2022-05-06 11:14:49 +0100

    Formatted message

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const core = require('@actions/core');
 // const github = require('@actions/github');
 const matrix = require('matrix-bot-sdk');
 
+function generateNoticeHtml(status) {
+    if (status == "Succeeded") {
+        const colour = "#00ff00";
+    } else {
+        const colour = "#ff0000";
+    }
+
+    const timestamp = new Date().toUTCString();
+    const branch = "branch";
+    const buildUrl = "https://bbc.co.uk";
+
+    return `GitHub Actions build for ${branch} <font color="${colour}">${status}</font> at <a href='${buildUrl}>${timestamp}</a>`;
+
+}
+
 try {
     const status = core.getInput("status");
     const matrixToken = core.getInput("token");
@@ -9,10 +24,9 @@ try {
     const homeserverUrl = core.getInput("homeserver");
 
     const client = new matrix.MatrixClient(homeserverUrl, matrixToken);
-    client.sendNotice(roomId, "Hello world").then(
+    client.sendHtmlNotice(roomId, generateNoticeHtml(status)).then(
         (eventId) => core.setOutput(eventId)
     );
-
 } catch (error) {
     core.setFailed(error.message);
 }
